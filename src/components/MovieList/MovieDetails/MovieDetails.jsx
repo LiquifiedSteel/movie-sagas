a278d34bfad74426180a7d423fbe9f0853ec4ccf
Alex Rivard <alex.rivard4406@gmail.com>
2024-10-04 16:11:23 -0500

    user can now travel back and forth between the list and details page

--- src/components/MovieList/MovieDetails/MovieDetails.jsx
+++ src/components/MovieList/MovieDetails/MovieDetails.jsx
@@ -1,11 +1,20 @@
-
+import { useSelector } from "react-redux";
+import { useEffect } from "react";
+import { Link } from "react-router-dom/cjs/react-router-dom.min";
 
 function MovieDetails() {
+    const movieId = useSelector(store => store.toDetailsReducer);
 
+    useEffect(() => {
+        const something = 0;
+    }, [])
 
     return (
         <>
-        
+            <header>
+                <h2>Movie Details</h2>
+                <Link to="/">Back to Movies List</Link>
+            </header>
         </>
     )
 }
--- src/redux/store.js
+++ src/redux/store.js
@@ -46,11 +46,21 @@ const genres = (state = [], action) => {
   }
 }
 
+const toDetailsReducer = (state = 0, action) => {
+  switch (action.type) {
+    case 'CAPTURE_MOVIE':
+      return action.payload;
+    default:
+      return state;
+  }
+}
+
 // Create one store that all components can use
 const storeInstance = createStore(
   combineReducers({
     movies,
     genres,
+    toDetailsReducer,
   }),
   // Add sagaMiddleware to our store
   applyMiddleware(sagaMiddleware, logger),
