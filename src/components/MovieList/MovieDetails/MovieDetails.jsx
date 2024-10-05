579b95a6e6b171d695d0e3c0a544ac751dcd99ea
Alex Rivard <alex.rivard4406@gmail.com>
2024-10-04 19:17:36 -0500

    MovieDetails functionality code has comments

--- src/components/MovieList/MovieDetails/MovieDetails.jsx
+++ src/components/MovieList/MovieDetails/MovieDetails.jsx
@@ -4,12 +4,13 @@ import { useHistory } from "react-router-dom/cjs/react-router-dom.min";
 
 
 function MovieDetails() {
-    const movieId = useSelector(store => store.toDetailsReducer);
-    const details = useSelector(store => store.detailsReducer)
-    const history = useHistory();
-    console.log(details);
+    const movieId = useSelector(store => store.toDetailsReducer); // movieId is used to tell the database which movie we want details for
+    const details = useSelector(store => store.detailsReducer); // details is an object of all the details for the desired movie
+    const history = useHistory(); // history is used for switching pages via routing
     const dispatch = useDispatch();
 
+    // this useEffect sends a request to the redux store asking for the details on the desired movie
+    // to be sent to the detailsReducer so the details can be grabbed by the details variable
     useEffect(() => {
         dispatch({type: "FETCH_DETAILS", payload: movieId});
     }, [])
